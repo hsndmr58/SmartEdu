ea553e6ad757d64a76eb2d4a6e1f748a7edf1e28
hasan <hasandemir.5802@gmail.com>
2021-11-10 23:38:27 +0300

    Ders 03

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const express = require("express");
+const mongoose =require("mongoose")
+const pageRoute=require('./routes/pageRoute')
+const courseRoute=require('./routes/courseRoute')
+
 
 const app = express();
 
 //Template Engine
 app.set("view engine","ejs")
 
+//Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+}).then(()=>{
+  console.log('DB Connected Successfuly')
+});
 
 //Middlewares
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 const port = 3000;
 //Roustes
-app.get("/", (req, res) => {
-    res.status(200).render('index',{
-      page_name:"index"
-    })
-});
-app.get("/about", (req, res) => {
-    res.status(200).render('about',{
-      page_name:"about"
-    })
-});
+app.use("/",pageRoute);
+app.use("/courses",courseRoute)
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
